5480d9cabfdc1129dbbb90ff7a092a705714964f
lómenoirë mortecc. <38693485+chaosarium@users.noreply.github.com>
2024-12-12 21:39:19 -0500

    part2

--- 2024/day12/bin/main.ml
+++ 2024/day12/bin/main.ml
@@ -47,51 +47,88 @@ let union (f: forest) (id1: int) (id2: int) : unit =
 
 type coord = int * int [@@deriving show]
 type direction = U | D | L | R [@@deriving show]
-type thing = Cell of coord | Wall of coord * direction [@@deriving show]
+type thing = Cell of coord | Wall of coord * direction | Corner of coord * direction [@@deriving show]
+(* 
+  L  U  U
+  L [X] R
+  D  D  R
+*)
+
+let components_per_cell = 9
 
 let enc (height: int) (width: int) (x: thing) = match x with
-  | Cell (i, j) -> (width * i + j ) * 5
-  | Wall ((i, j), L) -> (width * i + j ) * 5 + 1
-  | Wall ((i, j), U) -> (width * i + j ) * 5 + 2
-  | Wall ((i, j), R) -> (width * i + j ) * 5 + 3
-  | Wall ((i, j), D) -> (width * i + j ) * 5 + 4
+  | Cell (i, j) -> (width * i + j ) * components_per_cell
+  | Wall ((i, j), L) -> (width * i + j ) * components_per_cell + 1
+  | Wall ((i, j), U) -> (width * i + j ) * components_per_cell + 2
+  | Wall ((i, j), R) -> (width * i + j ) * components_per_cell + 3
+  | Wall ((i, j), D) -> (width * i + j ) * components_per_cell + 4
+  | Corner ((i, j), L) -> (width * i + j ) * components_per_cell + 5
+  | Corner ((i, j), U) -> (width * i + j ) * components_per_cell + 6
+  | Corner ((i, j), R) -> (width * i + j ) * components_per_cell + 7
+  | Corner ((i, j), D) -> (width * i + j ) * components_per_cell + 8
 
-let dec (height: int) (width: int) (id: int) = match id mod 5 with 
+let dec (height: int) (width: int) (id: int) = match id mod components_per_cell with 
   | 0 -> Cell (id / width, id mod width)
   | 1 -> let id = id - 1 in Wall ((id / width, id mod width), L)
   | 2 -> let id = id - 2 in Wall ((id / width, id mod width), U)
   | 3 -> let id = id - 3 in Wall ((id / width, id mod width), R)
   | 4 -> let id = id - 4 in Wall ((id / width, id mod width), D)
+  | 5 -> let id = id - 5 in Corner ((id / width, id mod width), L)
+  | 6 -> let id = id - 6 in Corner ((id / width, id mod width), U)
+  | 7 -> let id = id - 7 in Corner ((id / width, id mod width), R)
+  | 8 -> let id = id - 8 in Corner ((id / width, id mod width), D)
   | _ -> raise (Failure "impossible")
 
 (* algorithm *)
 
-let part1 (input: char array array): int = 
+(* returns id |-> num cells, num walls, num corners *)
+let mk_stats (input: char array array): (int, int * int * int) Hashtbl.t = 
   let height = Array.length input in
   let width = Array.length input.(0) in
-  let f = init_uf (5*width*height) in 
+  let f = init_uf (components_per_cell * width * height) in 
   let enc = enc height width in
   let dec = dec height width in
-  let checkWall i j = 
+  let union' thing1 thing2 = 
+    union f (enc thing1) (enc thing2) 
+  in
+  let checkWallAndCorner i j = 
+    let diffU = (i = 0)          || (input.(i-1).(j)) != (input.(i).(j))
+    and diffD = (i = height - 1) || (input.(i+1).(j)) != (input.(i).(j))
+    and diffL = (j = 0)          || (input.(i).(j-1)) != (input.(i).(j))
+    and diffR = (j = width - 1)  || (input.(i).(j+1)) != (input.(i).(j))
+    in
     (
-      if (i = 0)          || (input.(i-1).(j)) != (input.(i).(j)) then union f (enc (Cell(i,j))) (enc (Wall((i,j), U)));
-      if (i = height - 1) || (input.(i+1).(j)) != (input.(i).(j)) then union f (enc (Cell(i,j))) (enc (Wall((i,j), D)));
-      if (j = 0)          || (input.(i).(j-1)) != (input.(i).(j)) then union f (enc (Cell(i,j))) (enc (Wall((i,j), L)));
-      if (j = width - 1)  || (input.(i).(j+1)) != (input.(i).(j)) then union f (enc (Cell(i,j))) (enc (Wall((i,j), R)));
+      if diffU then union' (Cell(i,j)) (Wall((i,j), U));
+      if diffD then union' (Cell(i,j)) (Wall((i,j), D));
+      if diffL then union' (Cell(i,j)) (Wall((i,j), L));
+      if diffR then union' (Cell(i,j)) (Wall((i,j), R));
+      if diffL && diffU then (union' (Cell(i,j)) (Corner((i,j), L)));
+      if diffU && diffR then (union' (Cell(i,j)) (Corner((i,j), U)));
+      if diffR && diffD then (union' (Cell(i,j)) (Corner((i,j), R)));
+      if diffD && diffL then (union' (Cell(i,j)) (Corner((i,j), D)));
     )
   in
   let checkNeightbour i j =
+    let sameU = (i != 0)          && (input.(i-1).(j)) = (input.(i).(j))
+    and sameD = (i != height - 1) && (input.(i+1).(j)) = (input.(i).(j))
+    and sameL = (j != 0)          && (input.(i).(j-1)) = (input.(i).(j))
+    and sameR = (j != width - 1)  && (input.(i).(j+1)) = (input.(i).(j))
+    in
     (
-      if (i != 0)          && (input.(i-1).(j)) = (input.(i).(j)) then union f (enc (Cell(i,j))) (enc (Cell(i-1,j)));
-      if (i != height - 1) && (input.(i+1).(j)) = (input.(i).(j)) then union f (enc (Cell(i,j))) (enc (Cell(i+1,j)));
-      if (j != 0)          && (input.(i).(j-1)) = (input.(i).(j)) then union f (enc (Cell(i,j))) (enc (Cell(i,j-1)));
-      if (j != width - 1)  && (input.(i).(j+1)) = (input.(i).(j)) then union f (enc (Cell(i,j))) (enc (Cell(i,j+1)));
+      if sameU then union' (Cell(i,j)) (Cell(i-1,j));
+      if sameD then union' (Cell(i,j)) (Cell(i+1,j));
+      if sameL then union' (Cell(i,j)) (Cell(i,j-1));
+      if sameR then union' (Cell(i,j)) (Cell(i,j+1));
+      if sameL && sameU && (input.(i).(j)) != (input.(i-1).(j-1)) then (union' (Cell(i,j)) (Corner((i,j), L)));
+      if sameU && sameR && (input.(i).(j)) != (input.(i-1).(j+1)) then (union' (Cell(i,j)) (Corner((i,j), U)));
+      if sameR && sameD && (input.(i).(j)) != (input.(i+1).(j+1)) then (union' (Cell(i,j)) (Corner((i,j), R)));
+      if sameD && sameL && (input.(i).(j)) != (input.(i+1).(j-1)) then (union' (Cell(i,j)) (Corner((i,j), D)));
     )
   in
   let _ = 
     List.init height (function i -> 
       List.init width (function j -> 
-        (checkWall i j; checkNeightbour i j; ())
+        (checkWallAndCorner i j; checkNeightbour i j; ())
       )
     )
   in
@@ -103,15 +140,23 @@ let part1 (input: char array array): int =
           (let repr = find f (enc (Cell(i,j))) in 
           if f.(repr).size <= 1 then () else
           match Hashtbl.find_opt stats repr with 
-            | None -> Hashtbl.add stats repr (1, 0)
-            | Some (area, parameter) -> Hashtbl.replace stats repr (area+1, parameter));
+            | None -> Hashtbl.add stats repr (1, 0, 0)
+            | Some (area, parameter, corners) -> Hashtbl.replace stats repr (area+1, parameter, corners));
           
           (let _ = List.map (function dir -> 
             let repr = find f (enc (Wall((i,j), dir))) in 
             if f.(repr).size <= 1 then () else
             match Hashtbl.find_opt stats repr with 
-              | None -> Hashtbl.add stats repr (0, 1)
-              | Some (area, parameter) -> Hashtbl.replace stats repr (area, parameter+1)
+              | None -> Hashtbl.add stats repr (0, 1, 0)
+              | Some (area, parameter, corners) -> Hashtbl.replace stats repr (area, parameter+1, corners)
+            ) [L;R;U;D] in ());
+          
+          (let _ = List.map (function dir -> 
+            let repr = find f (enc (Corner((i,j), dir))) in 
+            if f.(repr).size <= 1 then () else
+            match Hashtbl.find_opt stats repr with 
+              | None -> Hashtbl.add stats repr (0, 0, 1)
+              | Some (area, parameter, corners) -> Hashtbl.replace stats repr (area, parameter, corners+1)
             ) [L;R;U;D] in ());
           
           ()
@@ -119,15 +164,30 @@ let part1 (input: char array array): int =
       )
     )
   in
+  stats
+
+let part1 input = 
+  let stats = mk_stats input in 
   let 
-    folder id (area, parameter) acc = acc + area * parameter
+    folder id (area, parameter, corners) acc = acc + area * parameter
   in
-  Hashtbl.fold folder stats 0
+    Hashtbl.fold folder stats 0
+
+let part2 input = 
+  let stats = mk_stats input in 
+  let 
+    folder id (area, parameter, corners) acc = acc + area * corners
+  in
+    Hashtbl.fold folder stats 0
   
 let () = Printf.printf "part1 test: %d\n" (part1 (parse "test.txt"))
 let () = Printf.printf "part1 test2: %d\n" (part1 (parse "test2.txt"))
 let () = Printf.printf "part1 test3: %d\n" (part1 (parse "test3.txt"))
 let () = Printf.printf "part1 test4: %d\n" (part1 (parse "test4.txt"))
 let () = Printf.printf "part1 real: %d\n" (part1 (parse "input.txt"))
-(* let () = Printf.printf "part2 test: %d\n" (part2 (parse "test.txt"))
-let () = Printf.printf "part2 real: %d\n" (part2 (parse "input.txt")) *)
+let () = Printf.printf "\n"
+let () = Printf.printf "part2 test: %d\n" (part2 (parse "test.txt"))
+let () = Printf.printf "part2 test2: %d\n" (part2 (parse "test2.txt"))
+let () = Printf.printf "part2 test3: %d\n" (part2 (parse "test3.txt"))
+let () = Printf.printf "part2 test4: %d\n" (part2 (parse "test4.txt"))
+let () = Printf.printf "part2 real: %d\n" (part2 (parse "input.txt"))
