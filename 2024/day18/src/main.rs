e570015da8a3348754c8c9fbd299d0d70d00d4af
lómenoirë mortecc. <38693485+chaosarium@users.noreply.github.com>
2024-12-23 13:13:05 -0500

    p1

---
+++ 2024/day18/src/main.rs
@@ -0,0 +1,70 @@
+#![allow(warnings)]
+
+use std::{fs, io};
+use rustc_hash::FxHashMap as HashMap;
+use rustc_hash::FxHashSet as HashSet;
+
+fn nbors(i: usize, j: usize, dim: usize, corrupted: &HashSet<(usize, usize)>) -> HashSet<(usize, usize)> {
+    let i = i as i64;
+    let j = j as i64;
+    let dim = dim as i64;
+    let res = [(i-1, j), (i+1, j), (i, j-1), (i, j+1)]
+        .into_iter().filter(|&(ii, jj)| {
+            ii >= 0 && jj >= 0 && ii < dim && jj < dim && (!corrupted.contains(&(ii as usize, jj as usize)))
+        })
+        .map(|(ii, jj)| (ii as usize, jj as usize))
+        .collect();
+    res
+}
+
+fn solve1(file_path: &str, prefix_len: usize, dim: usize) -> usize {
+    
+    let input_str = fs::read_to_string(file_path).expect("can't read");
+    
+    let mut corrupted: HashSet<(usize, usize)> = input_str.split("\n").filter(|&l| l != "")
+        .map(|l| {
+            let literals: Vec<&str> = l.split(",").collect();
+            let i: usize = literals[0].parse().unwrap();
+            let j: usize = literals[1].parse().unwrap();
+            (i, j)
+        })
+        .take(prefix_len)
+        .collect();
+    // println!("corrupted {:?}", &corrupted);
+    
+    
+    let mut seen: HashSet<(usize, usize)> = HashSet::default();
+    let start = (0, 0);
+    let target = (dim-1, dim-1);
+    
+    let mut depth = 0;
+    let mut frontier: HashSet<(usize, usize)> = HashSet::from_iter([start]);
+    while !frontier.is_empty() {
+        // println!("frontier {:?}", &frontier);
+        let mut new_frontier = HashSet::default();
+        for &(i_u, j_u) in frontier.iter() {
+            if (i_u, j_u) == target {
+                // println!("reached target at depth {}", depth);
+                return depth;
+            }
+            seen.insert((i_u, j_u));
+            let nbors_u = nbors(i_u, j_u, dim, &corrupted);
+            // println!("nbors_u {:?}", &nbors_u);
+            for &(i_v, j_v) in nbors_u.iter() {
+                if !seen.contains(&(i_v, j_v)) { 
+                    new_frontier.insert((i_v, j_v));
+                } 
+            }
+        }
+        depth += 1;
+        frontier = new_frontier;
+    }
+    
+    panic!("unreachable");
+
+}
+
+fn main() {
+    println!("p1 test: {}", solve1("test.txt", 12, 7));
+    println!("p1 real: {}", solve1("input.txt", 1024, 71));
+}
